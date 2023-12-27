8a18c04d6657b796aa3fdceb65c02850a07199de
isaksengeir <isaksengeir@hotmail.com>
2023-12-27 10:41:57 +0100

    test

---
+++ chat.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from openai import OpenAI
+
+# Sett opp API-nøkkelen din fra https://beta.openai.com/account/api-keys
+api_key = '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'
+client = OpenAI(api_key=api_key)
+
+def chat_with_gpt(prompt, model="gpt-3.5-turbo"):
+    response = client.chat.completions.create(model=model,
+    messages=[{"role": "user", "content": prompt}])
+    return response.choices[0].message["content"]
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("Du: ")
+        if user_input.lower() == 'exit':
+            break
+        response = chat_with_gpt(user_input)
+        print("GPT: " + response)
+
+
+
+
+
+
